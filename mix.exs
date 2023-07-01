16baddb0d861949e837ac17eaf37d002341e98a2
Seth Herr <seth.william.herr@gmail.com>
2023-07-01 09:23:55 -0700

    add TimeFormatter

--- mix.exs
+++ mix.exs
@@ -37,6 +37,7 @@ defmodule MayIsBikeMonth.MixProject do
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.19.0"},
@@ -50,7 +51,9 @@ defmodule MayIsBikeMonth.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:timex, "~> 3.0"},
+      {:tzdata, "~> 1.1"}
     ]
   end
 
